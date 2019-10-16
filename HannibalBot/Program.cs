5bcfd0528ac5ebdee126b436fbb31f3aeec0be53
Vincent Smith <VinceS22@gmail.com>
2019-10-16 14:37:49 -0400

    Committing basic bot with command structure. Based off of Discord.Net example #2

---
+++ HannibalBot/Program.cs
@@ -0,0 +1,89 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using HannibalBot.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HannibalBot
+{
+    class Program
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly LogSeverity logLevel = LogSeverity.Info;
+
+        // Discord.Net heavily utilizes TAP for async, so we create
+        // an asynchronous context from the beginning.
+        static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        //public Program()
+        //{
+        //    // It is recommended to Dispose of a client when you are finished
+        //    // using it, at the end of your app's lifetime.
+        //    _client = new DiscordSocketClient(new DiscordSocketConfig
+        //    {
+        //        LogLevel = logLevel
+        //    });
+
+        //    _client.Log += LogAsync;
+        //    _client.Ready += ReadyAsync;
+        //    _client.MessageReceived += MessageReceivedAsync;
+        //}
+
+        public async Task MainAsync()
+        {
+            // You should dispose a service provider created using ASP.NET
+            // when you are finished using it, at the end of your app's lifetime.
+            // If you use another dependency injection framework, you should inspect
+            // its documentation for the best way to do this.
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
+
+                client.Log += LogAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
+
+                // Tokens should be considered secret data and never hard-coded.
+                // We can read from the environment variable to avoid hardcoding.
+                await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("discordtoken"));
+                await client.StartAsync();
+
+                // Here we initialize the logic required to register our commands.
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+                await Task.Delay(-1);
+            }
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        // The Ready event indicates that the client has opened a
+        // connection and it is now safe to access the cache.
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+            return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .AddSingleton<PictureService>()
+                .BuildServiceProvider();
+        }
+    }
+}
